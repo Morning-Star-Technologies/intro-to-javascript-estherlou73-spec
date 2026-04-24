cb47107330bdc20e140edd5d33b70dd08a94881f
esther <estherlou73@gmail.com>
2026-04-24 03:31:53 +0000

    DC themed operators homework

--- homework/operators/operators.homework.js
+++ homework/operators/operators.homework.js
@@ -18,3 +18,28 @@
 */
 
 // Begin writing your code below
+
+/*Assignment Operators*/
+let nightwingHealth = 100;
+const jokerDamage = 25;
+nightwingHealth = nightwingHealth - jokerDamage;
+console.log('Nightwing is down by', jokerDamage, 'points and has', nightwingHealth, 'health left.');
+
+/*Comparison Operators*/
+let isNightwingAlive = nightwingHealth > 0;
+console.log('Is Nightwing alive?', isNightwingAlive);
+let isJokerStrongerThanNightwing = jokerDamage > nightwingHealth;
+console.log('Is the Joker stronger than Nightwing?', isJokerStrongerThanNightwing);
+
+/*Logical Operators*/
+let isNightwingInDanger = isNightwingAlive && isJokerStrongerThanNightwing;
+console.log('Is Nightwing in danger?', isNightwingInDanger)
+let shouldNightwingRunAway = isNightwingInDanger || nightwingHealth < 30;
+console.log('Should Nightwing run away?', shouldNightwingRunAway);
+
+/*String Operator*/
+let nightwingSecretIdentity = 'r' + 'i' + 'c' + 'h' + 'a' + 'r' + 'd' + ' '+ 'g' + 'r' + 'a' + 'y';
+console.log('Nightwing\'s secret identity is', nightwingSecretIdentity);
+
+/* was reading a DC teen titans graphic novel and decided to theme
+this off of it lol */
