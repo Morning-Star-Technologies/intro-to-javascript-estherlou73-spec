d7f3bc483af55cba8e2d096774d1b9b083d34ada
esther <estherlou73@gmail.com>
2026-05-01 03:00:41 +0000

    condionals homework

--- challenges/control-structures-conditionals/control-structures-conditionals.challenge.js
+++ challenges/control-structures-conditionals/control-structures-conditionals.challenge.js
@@ -6,3 +6,37 @@
 * 2. A switch statement with at least 3 cases and a default case that
 *    logs a message to the console for each case.
 */
+
+/* If/Else Boolean and Comparison Operator */
+
+let dickGraysonIsRobin = true;
+let dickGraysonWillBeNightwing = true;
+let nightwingStrength = 100;
+let robinStrength = 80;
+
+if (dickGraysonIsRobin === true) {
+    console.log('Dick Grayson is the current Robin')
+}
+
+if (nighwingStrength === 100 && robinStrength === 80 && nightwingStrength > robinStrength) {
+    console.log('Nightwing is stronger than Robin')
+} else {
+    console.log('Nightwing is not stronger than Robin')
+}
+
+/* Switch Statement */
+let batfamilyMember = 'Nightwing'
+
+switch (batfamilyMember) {
+    case 'Batman':
+        console.log('This is Bruce Wayne')
+        break
+    case 'Nightwing':
+        console.log('This is Dick Grayson')
+        break
+    case 'Robin':
+        console.log('This is Damian Wayne')
+        break
+    default:
+        console.log('This is not a recognized Bat-family member')
+}
