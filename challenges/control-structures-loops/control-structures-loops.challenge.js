cf9a8e971af69923fabbd5836341698997fec6c5
esther <estherlou73@gmail.com>
2026-05-08 11:58:24 +0000

    loops challenge

--- challenges/control-structures-loops/control-structures-loops.challenge.js
+++ challenges/control-structures-loops/control-structures-loops.challenge.js
@@ -5,3 +5,28 @@
 */
 
 // Begin writing your code below
+
+let teenTitans = ['Robin', 'Starfire', 'Cyborg', 'Raven', 'Beast Boy'];
+
+let i = 0;
+while (i < teenTitans.length) {
+  if (teenTitans[i].includes('e')) {
+    console.log(teenTitans[i]);
+  } else {
+    console.log('The names ' + teenTitans[i] + ' do not include the letter e.');
+  }
+    i++;
+}
+
+let dcHeroes = ['Superman', 'Batman', 'Wonder Woman'];
+
+let i = 0
+while (i < dcHeroes.length) {
+  if (dcHeroes[i].length > 6) {
+    console.log(dcHeroes[i]);
+  }
+    i++;
+}
+
+let teamCount = teenTitans.length > dcHeroes.length;
+console.log('Are there more Teen Titans than DC heroes?', teamCount);
