6712e2bc528f95fe2873a7046482e1cdc881d55b
esther <estherlou73@gmail.com>
2026-05-08 11:45:40 +0000

    loops homework

--- homework/control-structures-loops/control-structures-loops.homework.js
+++ homework/control-structures-loops/control-structures-loops.homework.js
@@ -9,3 +9,12 @@ let trueCount = 0;
 // Replace this comment with your code
 
 console.log('The number of true values is', trueCount);
+
+for (let i = 0; i < binaryValues.length; i++) {
+  if (binaryValues[i] === true) {
+    console.log(binaryValues[i]);
+  }
+}   
+
+trueCount = 7;
+console.log('The number of values listed as true is', trueCount);
